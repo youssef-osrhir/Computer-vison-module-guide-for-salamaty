933f21259778ee0940ffa024d68bb8fcec8317b4
youssef-osrhir <youssefosrhir@gmail.com>
2020-11-03 17:35:23 +0100

    First use case for mask detection

---
+++ Use-case-1(face-mask)/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+from maskDetector import VideoCamera 
+
+# Flask application
+
+app = Flask(__name__)
+app.run(debug=True)
+
+@app.route('/')
+def index():
+    #return "Hello"
+    return render_template('index.html')
+    
+    
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed', methods=['POST', 'GET'])
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    
+#to use IP webcam comment lines from 21 to 24 and remove commenting from lines 26 to 30.
+#@app.route('/video_feed/<address>', methods=['POST', 'GET'])
+#def video_feed(address):
+#return Response(gen(VideoCamera(address)),
+#                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', debug=True)
